c8aea127f3ecf62a7605b242e7b26f0810c843fd
maniebra <mani.ebra@gmail.com>
2025-01-10 22:01:14 +0330

    fixed video bg

--- cv-app/src/components/video-bg.jsx
+++ cv-app/src/components/video-bg.jsx
@@ -1,12 +1,13 @@
 export default function VideoBg() {
     return (
-        <video src="../public/generated_video.mp4"
-                controls={false}
+        <video controls={false}
                 autoPlay
                 muted
                 playsInline
                 loop
                 className="absolute inset-0 w-full h-full object-cover z-0"
-        />
+        >
+                <source src="../public/generated_video.mp4" type="video/mp4" />
+        </video>
     )
 }
